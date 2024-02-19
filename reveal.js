43ded6300f8c93a4befc82463ed32dfc1738097b
!--2B--! <brunobonetti.silva@gmail.com>
2024-02-19 18:58:51 -0300

    feat: create function in JS, scroll reveal

---
+++ reveal.js
@@ -0,0 +1,30 @@
+window.leftguide = ScrollReveal({reset:true})
+
+leftguide.reveal('.box__data', 
+{
+    origin: 'right',
+    distance: '100px',
+    duration: 500
+}),
+
+leftguide.reveal('.box__data1', 
+{
+    origin: 'right',
+    distance: '150px',
+    duration: 1000
+}),
+leftguide.reveal('.box__data2', 
+{
+    origin: 'right',
+    distance: '200px',
+    duration: 1500
+}),
+leftguide.reveal('.box__data3', 
+{
+    origin: 'right',
+    distance: '250px',
+    duration: 2000
+})
+
+
+
