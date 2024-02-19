2d53a0c5db48e8b169c967e6f666ab1e0d082bcd
!--2B--! <brunobonetti.silva@gmail.com>
2024-02-19 02:59:51 -0300

    feat: scripts landing page feature

---
+++ apiwpp.js
@@ -0,0 +1,9 @@
+import Typebot from 'https://cdn.jsdelivr.net/npm/@typebot.io/js@0.2/dist/web.js'
+
+Typebot.initBubble({
+  typebot: "meu-typebot-1-ykb9v3z",
+  theme: {
+    button: { backgroundColor: "#0042DA" },
+    chatWindow: { backgroundColor: "#d1d1d1" },
+  },
+});
