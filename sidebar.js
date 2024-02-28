0c750bb3b541178a32038d9f08855cbafb9596c0
!--2B--! <brunobonetti.silva@gmail.com>
2024-02-28 13:26:43 -0300

    feat: add transition sidebar

--- sidebar.js
+++ sidebar.js
@@ -8,13 +8,6 @@ document.addEventListener('DOMContentLoaded', function () {
       }
   
       buttonSideBar.addEventListener('click', () => {
-          openSidebar();
-      });
-  
-      hamburger.addEventListener('click', () => {
-          // Adicione um atraso antes de chamar openSidebar
-          setTimeout(() => {
-              openSidebar();
-          }, 10000); // Ajuste o valor de 300 para o tempo desejado em milissegundos
+        openSidebar()
       });
   });
