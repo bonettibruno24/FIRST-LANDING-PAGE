0391ec2e476793722ea357add8b50e823b48c36d
!--2B--! <brunobonetti.silva@gmail.com>
2024-03-10 03:27:57 -0300

    feat: save integrate typebot script corect

--- apiwpp.js
+++ apiwpp.js
@@ -1,9 +1,11 @@
 import Typebot from 'https://cdn.jsdelivr.net/npm/@typebot.io/js@0.2/dist/web.js'
 
 Typebot.initBubble({
-  typebot: "meu-typebot-1-ykb9v3z",
+  typebot: "my-typebot-vckj06u",
   theme: {
     button: { backgroundColor: "#0042DA" },
     chatWindow: { backgroundColor: "#d1d1d1" },
   },
 });
+
+
